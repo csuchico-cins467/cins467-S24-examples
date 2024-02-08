4659f4f906c8832a1ed810e648b9343ee9b46dc2
Bryan Dixon <javawolfpack@gmail.com>
2024-02-08 14:26:43 -0800

    Files

--- counterexample/lib/storage.dart
+++ counterexample/lib/storage.dart
@@ -1,22 +1,44 @@
-import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CounterStorage {
-  SharedPreferences? prefs;
   CounterStorage();
 
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
   Future<int> readCounter() async {
-    prefs ??= await SharedPreferences.getInstance();
-    return prefs!.getInt('counter') ?? 0;
-    // int? toReturn = prefs.getInt('counter');
-    // if (toReturn == null) {
-    //   return 0;
-    // }
-    // return toReturn;
+    try {
+      final file = await _localFile;
+      // Read the file
+      String contents = await file.readAsString();
+      return int.parse(contents);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      await writeCounter(0);
+    }
+    return 0;
   }
 
   Future<void> writeCounter(int counter) async {
-    prefs ??= await SharedPreferences.getInstance();
-    prefs!.setInt('counter', counter);
-    // write to the file
+    try {
+      final file = await _localFile;
+      await file.writeAsString('$counter');
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
   }
 }
